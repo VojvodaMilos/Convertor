92876d71a2a88e4fe376642ac2800151a08abc90
Milos1988 <vojvoda1988@gmail.com>
2022-10-24 13:35:01 +0200

    bez rsd-usd

--- src/App.js
+++ src/App.js
@@ -20,10 +20,13 @@ function App() {
       .then((data) => {
         setRsd(data.rates.RSD);
         setUsd(data.rates.USD);
+        console.log("radiiiiii")
       });
-  }, []);
-  console.log(inputLeftMoneu);
-  console.log(inputRightMoneu);
+  }, [inputLeft, inputRight, inputLeftMoneu, inputRightMoneu]);
+  // console.log(inputRightMoneu);
+
+  // console.log(inputLeftMoneu === "EUR");
+  // console.log(inputRightMoneu === "USD");
 
   return (
     <div className="converter">
@@ -35,6 +38,35 @@ function App() {
             onChange={(e) => {
               setInputLeftMoneu(e.target.value);
               // console.log(e.target.value);
+
+              if (sel1.current.value === "USD" && sel2.current.value === "EUR") {
+
+                setInputRight(inp1.current.value * usd);
+                inp2.current.value = inp1.current.value * usd;
+              }
+
+              if (sel1.current.value === "RSD" && sel2.current.value === "EUR") {
+
+                setInputLeft(inp1.current.value / rsd);
+                inp2.current.value = inp1.current.value / rsd;
+              }
+              if (sel1.current.value === "EUR" && sel2.current.value === "EUR") {
+                inp2.current.value = inp1.current.value;
+                // console.log("radi");
+              }
+
+
+              if (sel1.current.value === "USD" && sel2.current.value === "USD") {
+                inp2.current.value = inp1.current.value;
+                // console.log("radi");
+              }
+
+              if (sel1.current.value === "RSD" && sel2.current.value === "RSD") {
+                inp2.current.value = inp1.current.value;
+                // console.log("radi");
+              }
+
+
             }}
             name="monet"
             className="select"
@@ -61,6 +93,30 @@ function App() {
                 inp2.current.value = e.target.value;
                 // console.log("radi");
               }
+
+
+              if (sel1.current.value === "RSD" && sel2.current.value === "EUR") {
+                setInputRight(e.target.value / rsd);
+                inp2.current.value = e.target.value / rsd;
+              }
+
+              if (sel1.current.value === "USD" && sel2.current.value === "EUR") {
+
+                setInputLeft(inp1.current.value * usd);
+                inp2.current.value = inp1.current.value * usd
+              }
+              if (sel1.current.value === "USD" && sel2.current.value === "USD") {
+
+                setInputLeft(inp1.current.value);
+                inp2.current.value = inp1.current.value
+              }
+              if (sel1.current.value === "RSD" && sel2.current.value === "RSD") {
+
+                setInputLeft(inp1.current.value);
+                inp2.current.value = inp1.current.value
+              }
+
+
             }}
             type="number"
             className="input"
@@ -69,8 +125,31 @@ function App() {
         <img
           onClick={() => {
             // console.log(sel2.current.value);
-            setInputLeftMoneu(inp2.current.value);
-            setInputRightMoneu(inp1.current.value);
+
+            setInputLeftMoneu(sel2.current.value);
+            setInputRightMoneu(sel1.current.value);
+            let a = sel1.current.value
+            sel1.current.value = sel2.current.value
+            sel2.current.value = a
+            if (sel1.current.value === "RSD" && sel2.current.value === "EUR") {
+              setInputRight(inp1.current.value / rsd);
+              inp2.current.value = inp1.current.value / rsd;
+            }
+            if (sel1.current.value === "EUR" && sel2.current.value === "RSD") {
+              console.log("radi")
+              setInputRight(inp1.current.value * rsd);
+              inp2.current.value = inp1.current.value * rsd;
+            }
+            if (sel1.current.value === "USD" && sel2.current.value === "EUR") {
+              console.log("radi")
+              setInputRight(inp1.current.value * usd);
+              inp2.current.value = inp1.current.value * usd;
+            }
+            if (sel1.current.value === "EUR" && sel2.current.value === "USD") {
+              console.log("radi2222")
+              setInputRight(inp1.current.value / usd);
+              inp2.current.value = inp1.current.value / usd;
+            }
           }}
           src={img}
           alt=""
@@ -80,8 +159,40 @@ function App() {
           <select
             ref={sel2}
             onChange={(e) => {
+
+              if (sel1.current.value === "EUR" && sel2.current.value === "USD") {
+
+                setInputRight(inp2.current.value / usd);
+                inp2.current.value = inp1.current.value / usd;
+              }
+              if (sel1.current.value === "EUR" && sel2.current.value === "RSD") {
+
+                setInputRight(inp2.current.value * rsd);
+                inp2.current.value = inp1.current.value * rsd;
+              }
+
+              if (sel1.current.value === "USD" && sel2.current.value === "EUR") {
+
+                setInputRight(inp2.current.value * usd);
+                inp2.current.value = inp1.current.value * usd;
+              }
+              if (sel1.current.value === "USD" && sel2.current.value === "USD") {
+
+                setInputRight(inp2.current.value);
+                inp2.current.value = inp1.current.value;
+              }
+              if (sel1.current.value === "RSD" && sel2.current.value === "EUR") {
+
+                setInputRight(inp2.current.value / rsd);
+                inp2.current.value = inp1.current.value / rsd;
+              }
+              if (sel1.current.value === "EUR" && sel2.current.value === "EUR") {
+
+                setInputRight(inp1.current.value);
+                inp2.current.value = inp1.current.value;
+              }
+
               setInputRightMoneu(e.target.value);
-              // console.log(e.target.value);
             }}
             name="monet"
             className="select"
@@ -99,15 +210,27 @@ function App() {
                 setInputLeft(e.target.value / rsd);
                 inp1.current.value = e.target.value / rsd;
               }
+
               if (inputLeftMoneu === "EUR" && inputRightMoneu === "USD") {
-                // console.log("radi");
-                setInputLeft(e.target.value / rsd);
-                inp1.current.value = e.target.value / usd;
+                console.log("radi");
+                setInputLeft(e.target.value * usd);
+                inp1.current.value = e.target.value * usd;
               }
               if (inputLeftMoneu === "EUR" && inputRightMoneu === "EUR") {
                 console.log("radi");
                 inp1.current.value = e.target.value;
               }
+              if (sel1.current.value === "USD" && sel2.current.value === "USD") {
+
+                setInputRight(inp2.current.value);
+                inp1.current.value = inp2.current.value
+              }
+              if (sel1.current.value === "RSD" && sel2.current.value === "RSD") {
+
+                setInputRight(inp2.current.value);
+                inp1.current.value = inp2.current.value
+              }
+
             }}
             type="number"
             className="input"
@@ -115,7 +238,17 @@ function App() {
         </div>
       </div>
       <div className="res">
-        <button className="btn">Reset</button>
+        <button className="btn" onClick={() => {
+          inp1.current.value = 1
+          setInputLeft(1);
+          sel1.current.value = "EUR"
+          setInputLeftMoneu(sel1.current.value)
+          inp2.current.value = inp1.current.value * rsd
+          setInputRight(inp1.current.value * rsd);
+          sel2.current.value = "RSD"
+          setInputRightMoneu(sel2.current.value * rsd)
+        }
+        }>Reset</button>
       </div>
     </div>
   );
